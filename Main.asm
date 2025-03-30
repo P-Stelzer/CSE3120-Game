2f0d21e015df3576f65dca9ab6da0657e904e054
P-Stelzer <pstelzer2023@my.fit.edu>
2025-03-30 01:47:08 -0400

    random character generation in grid

--- Main.asm
+++ Main.asm
@@ -1,23 +1,41 @@
 INCLUDE Irvine32.inc
+
+
+
 .data
+Card STRUCT
+   symbol BYTE ?
+   state BYTE ? ; 0=hidden, 1=peek, 2=found
+Card ENDS
+
+DOT_CHAR EQU 249
+
 cursorX BYTE 0
 cursorY BYTE 0
+
+GRID_ROWS EQU 3
+GRID_COLS EQU 4
+GRID_ELEM_SIZE EQU TYPE Card
+grid DW GRID_ROWS * GRID_COLS DUP(?)
+
+
+
+
 .code
 
 MoveRight PROC
 
  mov dl, cursorX
  inc dl
- inc dl
 
-.IF dl >= 0 && dl <= 79
+.IF dl >= 0 && dl <= GRID_COLS
   mov al, " "
   call WriteChar
 
   mov dh, cursorY
   call Gotoxy
 
-  mov al, 249
+  mov al, DOT_CHAR
   call WriteChar
   call Gotoxy
 
@@ -34,16 +52,15 @@ MoveLeft PROC
 
  mov dl, cursorX
  dec dl
- dec dl
 
-.IF dl >= 0 && dl <= 79
+.IF dl >= 0 && dl <= GRID_COLS
   mov al, " "
   call WriteChar
 
   mov dh, cursorY
   call Gotoxy
 
-  mov al, 249
+  mov al, DOT_CHAR
   call WriteChar
   call Gotoxy
 
@@ -62,14 +79,14 @@ MoveUp PROC
  mov dh, cursorY
  dec dh
 
-.IF dh >= 0 && dh <= 25
+.IF dh >= 0 && dh <= GRID_ROWS
   mov al, " "
   call WriteChar
 
   mov dl, cursorX
   call Gotoxy
 
-  mov al, 249
+  mov al, DOT_CHAR
   call WriteChar
   call Gotoxy
 
@@ -87,14 +104,14 @@ MoveDown PROC
  mov dh, cursorY
  inc dh
 
-.IF dh >= 0 && dh <= 25
+.IF dh >= 0 && dh <= GRID_ROWS
   mov al, " "
   call WriteChar
 
   mov dl, cursorX
   call Gotoxy
 
-  mov al, 249
+  mov al, DOT_CHAR
   call WriteChar
   call Gotoxy
 
@@ -109,7 +126,36 @@ MoveDown ENDP
 
 main PROC
 
-mov al, 249
+   mov ecx, GRID_ROWS
+   mov ebx, 0 ; y
+
+ row_loop:
+   push ecx
+   mov ecx, GRID_COLS
+   mov esi, 0 ; x
+
+ col_loop:
+   mov eax, 26
+   call RandomRange
+   add al, 97
+   mov (Card PTR grid[ebx + esi * TYPE grid]).symbol, al
+
+
+   mov edx, esi
+   mov dh, bl
+   call Gotoxy
+   call WriteChar
+
+   inc esi
+   loop col_loop
+
+   inc ebx
+   pop ecx
+   loop row_loop
+
+
+
+mov al, DOT_CHAR
 call WriteChar
 
 mov dl, cursorX

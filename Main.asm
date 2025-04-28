057ffb1348d6e13381660b3523346d7d20ece34b
P-Stelzer <pstelzer2023@my.fit.edu>
2025-04-28 11:58:50 -0400

    refactoring and better skip

--- Main.asm
+++ Main.asm
@@ -6,7 +6,6 @@
 INCLUDE Irvine32.inc
 INCLUDE board.inc
 INCLUDE draw.inc
-;INCLUDE queue.inc
 
 
 
@@ -39,39 +38,46 @@ ENDM
 
 
 .const
-EASY_MODE GameProfile <0,4,7,14,28>
-NORMAL_MODE GameProfile <1,6,9,27,54>
-HARD_MODE GameProfile <2,9,16,72,144>
-
-.data
-
-game GameData <>
-
-cardRowPadding BYTE 2
-cardColPadding BYTE 4
-
-
 ; GRID_ROWS * GRID_COLS MUST BE EVEN AND NOT GREATER THAN 2*POOL_SIZE
 MAX_GRID_ROWS EQU 9
 MAX_GRID_COLS EQU 16
 MAX_NUM_SYMBOLS EQU (MAX_GRID_ROWS * MAX_GRID_COLS) / 2
 
+EASY_MODE GameProfile <0,4,7,14,28>
+NORMAL_MODE GameProfile <1,6,9,27,54>
+HARD_MODE GameProfile <2,9,16,72,144>
 
-grid Card MAX_GRID_ROWS * MAX_GRID_COLS DUP(<33,-1>)
 GRID_ORIGIN_X EQU 3
 GRID_ORIGIN_Y EQU 1
 
-
-randSymbols BYTE MAX_NUM_SYMBOLS*2 DUP(0)
+ROW_PADDING BYTE 2
+COL_PADDING BYTE 4
 
 POOL_SHUFFLES EQU 3
 BOARD_SHUFFLES EQU 3
 
+WELC_STR_1 BYTE "Welcome to Memory Matching!...",0
+WELC_STR_2 BYTE "Select cards with the arrow keys...",0
+WELC_STR_3 BYTE "Reveal the selected card with the space bar...",0
+WELC_STR_4 BYTE "Each card has exactly one match...",0
+WELC_STR_5 BYTE "If the last two revealed cards match, they remain visible...",0
+WELC_STR_6 BYTE "Find all matches to win!...",0
 
-infoStr1 BYTE "Attempted Matches: ",0
-infoStr2 BYTE "Matches Remaining: ",0
+DIFF_PROMPT BYTE "Choose Difficulty:",0
 
-winMessage BYTE "You matched all the cards!",0
+INFO_STR_1 BYTE "Attempted Matches: ",0
+INFO_STR_2 BYTE "Matches Remaining: ",0
+
+WIN_MESSAGE BYTE "You matched all the cards!",0
+
+
+
+.data
+game GameData <>
+
+grid Card MAX_GRID_ROWS * MAX_GRID_COLS DUP(<33,-1>)
+
+randSymbols BYTE MAX_NUM_SYMBOLS*2 DUP(0)
 
 ; pool declaration must be fragmented like this else the rest of the memory tweaks
 POOL_SIZE EQU 73
@@ -135,13 +141,13 @@ DrawBoard PROC USES ecx ebx edi eax esi
    ; Calculate Location
    mov edx, esi ; set dl == x
    mov al, dl
-   mul cardColPadding
+   mul COL_PADDING
    add al, GRID_ORIGIN_X
    mov dl, al
 
    mov dh, bl   ; set dh == y
    mov al, dh
-   mul cardRowPadding
+   mul ROW_PADDING
    add al, GRID_ORIGIN_Y
    mov dh, al
 
@@ -174,7 +180,7 @@ DrawInfo PROC USES eax edx
    mov dl, GRID_ORIGIN_X
    mov dh, GRID_ORIGIN_Y
    getProfileField al, gridRows
-   mul cardRowPadding
+   mul ROW_PADDING
    add al, 2
    add dh, al
 
@@ -182,7 +188,7 @@ DrawInfo PROC USES eax edx
 
    call Gotoxy
    
-   mov EDX, OFFSET infoStr1
+   mov EDX, OFFSET INFO_STR_1
 
    call WriteString
 
@@ -200,11 +206,11 @@ DrawInfo PROC USES eax edx
    getProfileField al, numSymbols
    mov ah, game.numFound
    .IF ah >= al
-      mov EDX, OFFSET winMessage
+      mov EDX, OFFSET WIN_MESSAGE
       call WriteString
    .ELSE
       push edx
-      mov EDX, OFFSET infoStr2
+      mov EDX, OFFSET INFO_STR_2
       call WriteString
 
       ; Clear decimal field so that digits are not left behind
@@ -213,7 +219,7 @@ DrawInfo PROC USES eax edx
       call WriteChar
 
       pop edx
-      add dl, LENGTHOF infoStr2
+      add dl, LENGTHOF INFO_STR_2
       dec dl
       call Gotoxy
 
@@ -227,16 +233,6 @@ DrawInfo PROC USES eax edx
 
 DrawInfo ENDP
 
-
-; === mShowWelc =====================================================================
-mShowWelc MACRO text:REQ, x:REQ, y:REQ, delay:=<30>
-   mPrintMessage x,y,delay,text
-   ;mHideCursor
-   call ReadChar
-   .IF AX == 2960h
-      jmp game_start
-   .ENDIF
-ENDM
 
 
 ; === mPeekCard =====================================================================
@@ -299,20 +295,7 @@ LOCAL loop_start
 ENDM
 
 
-; === mHideCursor ===================================================================
-mHideCursor MACRO
 
-   push edx
-   push eax
-   call GetMaxXY
-   mov dh, al
-   dec dh
-   dec dl
-   call GotoXY
-   pop eax
-   pop edx
-
-ENDM
 
 
 mChooseDifficulty MACRO x:REQ, y:REQ
@@ -387,16 +370,41 @@ ENDM
 ; === MAIN ==========================================================================
 main PROC
 
-   mShowWelc <"Welcome to Memory Matching!...">, GRID_ORIGIN_X, GRID_ORIGIN_Y
-   mShowWelc <"Select cards with the arrow keys...">, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+3)
-   mShowWelc <"Reveal the selected card with the space bar...">, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+5)
-   mShowWelc <"Each card has exactly one match...">, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+8)
-   mShowWelc <"If the last two revealed cards match, they remain visible...">, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+10)
-   mShowWelc < "Find all matches to win!...">, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+13)
+; === mShowWelc =====================================================================
+mShowWelc MACRO text:REQ, x:REQ, y:REQ, delay:=<30>
+   mPrintMessage x,y,delay,text
+   ;mHideCursor
+   call ReadChar
+   .IF AX == 2960h
+      jmp skip_intro
+   .ENDIF
+ENDM
 
-game_start:
-nop
-   mPrintMessage GRID_ORIGIN_X, %(GRID_ORIGIN_Y+16),30,<"Choose Difficulty:">, 
+   mShowWelc WELC_STR_1, GRID_ORIGIN_X, GRID_ORIGIN_Y
+   mShowWelc WELC_STR_2, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+3)
+   mShowWelc WELC_STR_3, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+5)
+   mShowWelc WELC_STR_4, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+8)
+   mShowWelc WELC_STR_5, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+10)
+   mShowWelc WELC_STR_6, GRID_ORIGIN_X, %(GRID_ORIGIN_Y+13)
+   jmp game_start
+
+  skip_intro:
+   mGotoXY GRID_ORIGIN_X, GRID_ORIGIN_Y
+   mWriteString WELC_STR_1
+   mGotoXY GRID_ORIGIN_X, %(GRID_ORIGIN_Y+3)
+   mWriteString WELC_STR_2
+   mGotoXY GRID_ORIGIN_X, %(GRID_ORIGIN_Y+5)
+   mWriteString WELC_STR_3
+   mGotoXY GRID_ORIGIN_X, %(GRID_ORIGIN_Y+8)
+   mWriteString WELC_STR_4
+   mGotoXY GRID_ORIGIN_X, %(GRID_ORIGIN_Y+10)
+   mWriteString WELC_STR_5
+   mGotoXY GRID_ORIGIN_X, %(GRID_ORIGIN_Y+13)
+   mWriteString WELC_STR_6
+
+  game_start:
+   nop
+   mPrintMessage GRID_ORIGIN_X, %(GRID_ORIGIN_Y+16),30,DIFF_PROMPT
    mChooseDifficulty %(GRID_ORIGIN_X+2), 18
 
 
@@ -425,49 +433,37 @@ nop
 
    getProfileField dl, gridRows
    getProfileField dh, gridCols
-   mFillCards grid, dl, dh
+   mFillCards grid, randSymbols, dl, dh
 
 ; GAME START
 
    call Clrscr
 
-mBoardFlourish MACRO
-LOCAL numRows, numCols, q, i, loop1, loop2
+
+mLoadBoard MACRO
+LOCAL loop_start, delay
 .data
-   numRows BYTE 0
-   numCols BYTE 0
-   q QUEUE <>
-   i DWORD 0
-
+   delay DWORD ?
 .code
-   getProfileField al, gridRows
-   mov numRows, al
+   mov eax, white
+   call SetTextColor
 
-   getProfileField al, gridCols
-   mov numRows, al
+   getProfileField al, id
 
-  loop2:
-   INVOKE Enqueue, q, i
-   mov ecx, 0
-  loop1:
-   INVOKE Dequeue q
-   nop
-   inc eax
-   INVOKE Enqueue, q, eax
-   inc ecx
-   cmp ecx, i
-   jb loop1
-   inc i
-   cmp i, numCols
-   jb loop2
+   .IF al == 0
+      mov delay, 40
+   .ELSEIF al == 1
+      mov delay, 25
+   .ELSEIF al == 2
+      mov delay, 10
+   .ELSE
+      nop
+   .ENDIF
 
-
-
-ENDM
 
    mov edi, 0
 
-  abcdefg:
+  loop_start:
    mov ax, di
    getProfileField bl, gridCols
    div bl
@@ -475,12 +471,12 @@ ENDM
    mov dl, ah
 
    mov al, dl
-   mul cardColPadding
+   mul COL_PADDING
    add al, GRID_ORIGIN_X
    mov dl, al
 
    mov al, dh
-   mul cardRowPadding
+   mul ROW_PADDING
    add al, GRID_ORIGIN_Y
    mov dh, al
 
@@ -489,15 +485,17 @@ ENDM
    call WriteChar
 
    pushad
-   INVOKE Sleep, 40	;; honk shoe
+   INVOKE Sleep, delay	;; honk shoe
    popad
 
    inc edi
    mov eax, 0
    getProfileField ax, numCards
    cmp edi, eax
-   jl abcdefg
+   jl loop_start
    
+ENDM
+   mLoadBoard
 
 
 
